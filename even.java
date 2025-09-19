12c0204d472205f5f5c31474a454c89ed9037730
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-19 11:32:20 +0530

    done

---
+++ even.java
@@ -0,0 +1,10 @@
+public class even {
+    public static void main(String[] args) {
+        int n = 15; // Example number
+        if (n % 2 == 0) {
+            System.out.println(n + " is even.");
+        } else {
+            System.out.println(n + " is odd.");
+        }
+    }
+}
