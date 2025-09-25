f86c1b02bf853c616467a4fab8d67d1d45e48a6f
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-25 10:49:21 +0530

    array

---
+++ demo.java
@@ -0,0 +1,15 @@
+//public class arrays {
+    //public static void main(String[] args) {
+        //int [] arrays= {1,2,3,4,5,};
+      //  System.out.println(arrays.length  );
+    //}
+//}
+public class demo {
+    demo{
+        System.out.println("helllpodik");
+    }
+}
+
+// types of constructor --- 1.default 2.parameterized 3.Copy constructar.
+// dafault -- a constructor that haave no parameter.
+//implicit default- creat byself/ explicit- if we define constructor.
