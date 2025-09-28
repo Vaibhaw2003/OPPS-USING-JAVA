149abff91708bfeafff1f9e2e4e611c41c0fd190
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-09-28 10:22:45 +0530

    grade

--- JAVA_COMPLETE/person.java
+++ JAVA_COMPLETE/person.java
@@ -81,18 +81,67 @@
 
 
 // ADITON OF TWO NUMBER GIVEN FROM THE USER ----------
+// import java.util.Scanner;
+// public class person {
+
+//     public static void main(String[] args){
+
+//         int a,b;
+//         System.out.println("enter the value of a: ");
+//         System.out.println("enter the value of b: ");
+//         Scanner obj = new Scanner(System.in);
+//         a= obj.nextInt();
+//         b = obj.nextInt();
+//         int sum =(a+b);
+//         System.out.println("sume of a+b : " + sum);
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class person {
+
+//     public static void main(String[] args){
+//         int a,b;
+//         System.out.println("enter the value of a: ");
+//         System.out.println("enter the value of b: ");
+//         Scanner obj = new Scanner(System.in);
+//         a= obj.nextInt();
+//         b = obj.nextInt();
+//         int mul = a*b;
+//         System.out.println("multiplication of two number is : "+ mul);        
+//     }
+// }
+
+
+
+
+//getting marks from the user to pirnt grade ----------
+
 import java.util.Scanner;
 public class person {
 
     public static void main(String[] args){
 
-        int a,b;
-        System.out.println("enter the value of a: ");
-        System.out.println("enter the value of b: ");
-        Scanner obj = new Scanner(System.in);
-        a= obj.nextInt();
-        b = obj.nextInt();
-        int sum =(a+b);
-        System.out.println("sume of a+b : " + sum);
+        int marks;
+        System.out.println("enter your marks: ");
+        Scanner obj =new Scanner(System.in);
+        marks = obj.nextInt();
+
+        if (marks>=90){
+            System.out.println("A+");    
+        }
+        else if(marks>=70){
+            System.out.println("A");
+        }
+        else if(marks >=50){
+            System.out.println("B");
+        }
+        else if(marks >=35){
+            System.out.println("C");
+        }
+        else{
+            System.out.println("fail");
+        }
     }
 }
