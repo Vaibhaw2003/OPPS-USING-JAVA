abae21f22343a4be7885633b4e2c98bc93b8d69e
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-11-16 12:54:37 +0530

    file handling

--- UNIT02/WriteFile.java
+++ UNIT02/WriteFile.java
@@ -1,13 +1,69 @@
-import java.io.*;
+// import java.io.*;
 
-public class WriteFile {
-    public static void main(String[] args) {
-        try {
-            FileWriter fw = new FileWriter("test.txt");
-            fw.write("Hello Java!");
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("Writing error.");
-        }
-    }
+// public class WriteFile {
+//     public static void main(String[] args) {
+//         try {
+//             FileWriter fr = new FileWriter("test.txt");
+//             fr.write("Hello Java!");
+//             fr.close();
+//         } catch (IOException e) {
+//             System.out.println("Writing error.");
+//         }
+//     }
+// }
+
+//package UNIT02;
+
+// public class WriteFile {
+//     public static void main(String[] args) {
+//         System.out.println("WriteFile class in UNIT02 package.");
+//         // Your code to write to a file goes here
+//     }
+// }
+
+
+
+// public class WriteFile {
+
+//     public static void main(String[] args) {
+    
+//         int a = 10;
+//         int b = 20;
+//         int div = a / b;
+//         try{
+//             System.out.println("the div of a and b is : " + div);
+//         }catch(Exception exception){
+//             System.out.println("Exception occured: " + exception);
+//         }finally{
+//             System.out.println("This block is always executed.");
+//         }
+//     }
+// }
+
+
+
+// file handling create  
+
+// import java.io.*;
+// public class CreateFile{
+//     public static void main(String[] args){
+//         try{
+//             File fc = new File("myfile.txt");
+
+//             if (fc.createNewFile()) {
+//                 System.out.println("File created: "); 
+//             }else {
+//                 System.out.println("File already exists.");
+//             }
+//         }catch(IOException e){
+//             System.out.println("error occured:" + e );
+//         }
+//     }
+// }
+
+
+// file handling write
+import java.io.*;
+public class writeFile{
+    
 }
