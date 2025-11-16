8e04a783f85f2c0e9393a93fe49425b517621132
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-11-17 01:49:46 +0530

    ABTRACTION

--- UNIT02/supers.java
+++ UNIT02/supers.java
@@ -1,19 +1,86 @@
-class Animal {
-    void eat(){
-         System.out.println("animal eat food");
-    } 
-}
- class Snake extends Animal {
-    void poo(){
-        System.out.println("snake can pooo");
-    } 
+// class Animal {
+//     void eat(){
+//          System.out.println("animal eat food");
+//     } 
+// }
+//  class Snake extends Animal {
+//     void poo(){
+//         System.out.println("snake can pooo");
+//     } 
+// }
+
+
+// public class supers {
+//     public static void main(String[] args){
+//         Snake s = new Snake();
+//         s.eat();
+//         s.poo();
+//     }
+// }  
+
+
+//Methode overloading....
+
+// class Calculatore {
+//     int add(int a, int b){
+//         return a + b;
+//     }
+//     float add(float a, float b){
+//         return a + b;
+//     }
+//     double add ( double a,  double b){
+//         return a + b;
+//     }
+// }
+// public class supers {
+//     public static void main(String[] args){
+//         Calculatore c = new Calculatore();
+//         System.out.println(c.add(12, 200)); 
+//         System.out.println(c.add(1.2, 1.33));
+//         System.out.println(c.add(1.2, 13.3));
+//     }
+// }
+
+
+//Method OVERRIDING.....
+
+// class Animal {
+//     void eat(){
+//         System.out.println("animal can eat");
+//     }
+// }
+
+// class Dog extends Animal {
+//     @Override
+//     void eat(){
+//         System.out.println("dog can also eat");
+//     }
+// }
+
+// public class supers {
+//     public static void main(String[] arg){
+//         Animal a = new Dog();
+
+//         a.eat();
+//      }
+// }  
+
+
+// INCAPSULATION.....
+
+class Bank {
+    Private int bal = 1000;
+
+    public int
 }
 
 
-public class supers {
-    public static void main(String[] args){
-        Snake s = new Snake();
-        s.eat();
-        s.poo();
-    }
-}  
+
+
+
+// ABTRACTION ......
+
+
+
+
+
