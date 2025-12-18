de636641399c2226f0e36a916df4670d847b235b
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-18 23:56:52 +0530

    LinkedList

--- UNIT05/Collection.java
+++ UNIT05/Collection.java
@@ -1,7 +1,7 @@
 //-------------[ Collection Framework ]-----------
 
 //----------[01] ArrayList -------------
-
+/*  
 import java.util.ArrayList;   // ArrayList ko use karne keliye utill package ko istall karna hoga.
 
 class Collection {
@@ -15,3 +15,50 @@ class Collection {
 
     }
 }
+*/
+
+
+//----------[ remove ]----------
+
+/*  
+import java.util.ArrayList;
+
+class Collection {
+    public static void main(String[] args){
+        ArrayList<Integer> age  = new ArrayList<>();
+
+        age.add(22);
+        age.add(33);
+
+        System.out.println(age);
+        age.remove(0);
+        System.out.println(age);
+
+    }
+}
+*/
+
+
+// --------[ set ]--------( yah valuse ko replesh kar deta hai ).
+/*  
+import java.util.ArrayList;
+
+class Collection {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>();
+        name.add("vaibhaw singh");
+        name.add("harsh singh");
+        name.add("marco");
+
+        System.out.println(name);
+        name.set(1, "birendra");
+        name.remove(0);
+        System.out.println(name.get(1)); // name.get ( se hame jisko bhi print karana chahte ho ushka index de ke print kara sakte hai.)
+        name.clear();// ye methode sab kuch arraay ke element ko clear kar dega.
+        System.out.println(name);
+
+        }
+}
+*/
+
+//------------[ LinkedList ]----------
