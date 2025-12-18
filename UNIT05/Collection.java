fb6b82b8597ae3a492f117d60e2c55a46c55d088
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2025-12-18 23:34:20 +0530

    Collection Framework

---
+++ UNIT05/Collection.java
@@ -0,0 +1,17 @@
+//-------------[ Collection Framework ]-----------
+
+//----------[01] ArrayList -------------
+
+import java.util.ArrayList;   // ArrayList ko use karne keliye utill package ko istall karna hoga.
+
+class Collection {
+    public static void main(String[] ags){
+       ArrayList<String> name = new ArrayList<>();
+
+       name.add("vaibhaw singh ");
+       name.add("marco");
+
+       System.out.println(name);
+
+    }
+}
