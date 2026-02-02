54caa54e30241b0eddf94c5c23540f134a74646a
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-02-02 21:49:17 +0530

    JAVA CLASS 01

---
+++ first java/Str.java
@@ -0,0 +1,55 @@
+//------------ JAVA PROGRAM ------------//
+
+// A simple Java program that prints "Hello World" ---------
+
+// public class Str {
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
+// }
+
+// A simple Java program that demonstrates exception handling ---------
+
+// public class Str {
+//     public static void main(String[] args){
+//         int i = 10;
+//         do{
+//             System.out.println("The value of i is: " + i);
+//             i--;
+//         }
+//         while(i > 0);{
+//             System.out.println("Loop ended.");
+//         }
+//     }
+// }
+
+// A simple Java program FOR LOOP ---------
+
+// import java.util.Scanner;
+// public class Str{
+//     public static void main(String[]args){
+//         int n;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         n = sc.nextInt();
+//         for(int i=1; i<=n; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// A simple Java program WHILE LOOP ---------
+import java.util.Scanner;
+class Str{
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        n = sc.nextInt();
+        int i= 1;
+        while (i<=n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
