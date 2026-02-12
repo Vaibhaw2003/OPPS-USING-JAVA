910c2355f2a44b51f86ae0c1072075591efb2d01
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-02-13 03:27:17 +0530

    Mapping Streams

--- ADV-UNIT02/Demo.java
+++ ADV-UNIT02/Demo.java
@@ -89,23 +89,35 @@
 
 //         List<Integer> numbers = List.of(129, 20, 30);
 //         numbers.stream().map(n -> n%2==0).forEach(System.out::println);
+
+//         List<Integer>arr = List.of(1, 2, 3, 4, 5);
+//         arr.stream().map(n -> n * n).forEach(System.out::println);
+
+//         List<String> names2 = List.of("Don", "Raj", "Aman", "Vijay");
+//         names2.stream().map(n -> n.length()).forEach(System.out::println);
 //     }
 // }
 
 //--------- Reducing Streams ---------
 
-import java.util.List;
-class Demo{
-    public static void main(String[] args) {
-        List<Integer> numbers = List.of(10, 20, 30);
-        numbers.stream().forEach(System.out::println);
+// import java.util.List;
+// class Demo{
+//     public static void main(String[] args) {
+//         List<Integer> numbers = List.of(10, 20, 30);
+//         numbers.stream().forEach(System.out::println);
 
-        List<Integer>arr = List.of(1, 2, 3, 4, 5);
-        int sum = arr.stream().reduce(0, (a, b) -> a + b);
-        System.out.println("Sum: " + sum);
+//         List<Integer>arr = List.of(1, 2, 3, 4, 5);
+//         int sum = arr.stream().reduce(0, (a, b) -> a + b);
+//         System.out.println("Sum: " + sum);
 
-            int product = arr.stream().reduce(1, (a, b) -> a * b);
-        System.out.println("Product: " + product);
-        
-    }
-}
+//             int product = arr.stream().reduce(1, (a, b) -> a * b);
+//         System.out.println("Product: " + product);
+
+//         int max = arr.stream().reduce(arr.get(0), (a, b) -> a > b ? a : b);
+//         System.out.println("Max: " + max);
+
+//        int div = arr.stream().reduce(2,(a,c )-> a/c);
+//        System.out.println("div:"+ div);
+
+//     }
+// }
