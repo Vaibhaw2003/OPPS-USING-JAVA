786800a2f67ef579d80cf3d847e333c0c35b2d94
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-02-19 22:12:50 +0530

    OOPs Concepts

---
+++ UNIT02/Demo.java
@@ -0,0 +1,7 @@
+//------------- OOPS ----------
+//Core OOPs Concepts (4 Pillars)
+// Encapsulation (Data Hiding)  ||  Inheritance (Code Reusability) || Polymorphism (Many Forms) || Abstraction (Hiding Complexity) .
+
+class Demo {
+    
+}
--- UNIT02/calculatore.java
+++ UNIT02/calculatore.java
@@ -1,19 +1,65 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class Demo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number : ");
-        int num1 = sc.nextInt();
-        System.out.println("enter the second numbe : ");
-        int num2 =sc.nextInt();
+// class Demo {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the number : ");
+//         int num1 = sc.nextInt();
+//         System.out.println("enter the second numbe : ");
+//         int num2 =sc.nextInt();
 
-        int sum = num1 + num2;
-        int mul = num1 * num2;
+//         int sum = num1 + num2;
+//         int mul = num1 * num2;
 
-        System.out.println("sum : "+ sum);
-        System.out.println("mul :" + mul);
+//         System.out.println("sum : "+ sum);
+//         System.out.println("mul :" + mul);
 
-    }
-}
+//     }
+// }
+
+// --------------- INHARITANCE -----------
+
+// class Dem {
+//     void show(){
+//         System.out.println("animal can eat ");
+//     }    
+// }
+
+// class Singh  extends Dem{
+//     void king(){
+//         System.out.println("singh is king ");
+//     }
+// }
+
+// class calculatore{
+//     public static void main(String[] args){
+//         Singh s = new Singh();
+//         s.king();
+//         s.show();
+
+//     }
+// }
+
+
+//---------------- calculatore ----------------
+
+// class cal{
+//     void sum(int a, int b){
+//         System.out.println("sum of two number is : " + a +b );
+//     }
+// }
+
+// class mul extends cal{
+//     void multi(int a, int b){
+//         System.out.println("multilply of two number is : " + a*b);
+//     }
+// }
+
+// class calculatore {
+//     public static void main(String[] args) {
+//         mul m = new mul();
+//         m.sum(12, 34);
+//         m.multi(23,23);
+//     }
+// }
 
