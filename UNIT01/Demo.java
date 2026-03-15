ffb93f3e84ffa0def1a721e3534b73c5c814e85e
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-15 12:59:49 +0530

    loop

--- UNIT01/Demo.java
+++ UNIT01/Demo.java
@@ -187,4 +187,28 @@ class Demo {
 //     }
 // }
 
+// ---------- while loop || do while loop ---------
 
+// class Demo {
+//     public static void main(String[] args) {
+//         int i = 0;
+//         while (i<=10) {
+//             System.out.println("ram ram ");
+//             i++;
+            
+//         }
+//     }
+// }
+
+//--------- do while loop -------
+
+class Demo {
+    public static void main(String[] args) {
+        int i=0;
+        do{
+            System.out.println("ram");
+            i++;
+        }
+        while(i <=4);
+    }
+}
