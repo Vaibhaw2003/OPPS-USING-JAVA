201946c4c959c2e4dd5e121ad62fbe848a1c14cf
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-02 23:05:28 +0530

    CONSTRUCTOR

--- opps_2/Demo.java
+++ opps_2/Demo.java
@@ -17,3 +17,38 @@
 //         System.out.println("Course: " + s.course);
 //     }
 // }
+
+//-------- CONSTRUCTOR ---------
+// a constructor is a special method that is called when an object is instantiated. It is used to initialize the object.
+
+class Student{
+    String name;
+    int age;
+    String course;
+
+    // constructor
+    Student(String name, int age, String course){
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+}
+//
+class Demo {
+    public static void main(String[] args) {
+        Student s = new Student("vaibhaw", 20, "BCA");
+
+        System.out.println("Name: " + s.name);
+        System.out.println("Age: " + s.age);
+        System.out.println("Course: " + s.course);
+    }
+}
+
+
+
+
+
+//-------- inheritance ---------
+//-------- abstraction ---------
+//-------- encapsulation ---------
+//-------- polymorphism ---------
