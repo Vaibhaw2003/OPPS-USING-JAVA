e4c2ee807e75265af760178afecae21deb78abbd
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-04 23:09:42 +0530

    Lambda Expression to filter maximum number

--- ADV-UNIT01/Demo.java
+++ ADV-UNIT01/Demo.java
@@ -580,3 +580,17 @@
 //          l.stream().filter(x-> x%2==0).forEach(System.out::println);
 //     }
 // }
+
+
+//---------- Lambda Expression to filter maximum number from a list ---------
+
+// import java.util.Arrays;
+// import java.util.List;
+// class Demo{
+//     public static void main(String[] args) {
+//         List<Integer> n = Arrays.asList(10,20,5,40);
+//         n.stream()
+//         .filter(x -> x > 20)
+//         .forEach(System.out::println);
+//     }
+// }
