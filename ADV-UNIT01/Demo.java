ba00c3c58075278ee3691a15614bdb3cd587e5af
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-04 23:04:10 +0530

    Lambda Expression to filter even numbers

--- ADV-UNIT01/Demo.java
+++ ADV-UNIT01/Demo.java
@@ -567,3 +567,16 @@
 //         System.out.println(p.test(21));
 //     }
 // }
+
+//---------- Lambda Expression to filter even numbers from a list ---------
+
+// import java.util.Arrays;
+// import java.util.List;
+// class Demo{
+//     public static void main(String[] args) {
+//        List<Integer> l = Arrays.asList(1,2,3,4,5,6);
+//        //l.stream().forEach(System.out::println);
+
+//          l.stream().filter(x-> x%2==0).forEach(System.out::println);
+//     }
+// }
