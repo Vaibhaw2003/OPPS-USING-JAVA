dae685fcfbb41457005ae11500079ebb175a4a6a
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-18 02:18:53 +0530

    POLYMORPHISM

--- UNIT01/Demo.java
+++ UNIT01/Demo.java
@@ -271,3 +271,28 @@ class Demo {
 
 //     }
 // }
+
+//------- POLYMORPHISM ---------
+
+// Method Overloading (Compile-time Polymorphism)
+
+// class Cal {
+//     int sum(int a , int b){
+//         return a + b;
+//     }
+//     double sum(double a , double b){
+//         return a + b;
+//     }
+//      int sum(int a , int b, int c){
+//         return a + b + c;
+//     }
+// }
+// class Demo{
+//     public static void main(String[] args) {
+//         Cal obj = new Cal();
+//         System.out.println("Sum of 2 integers: " + obj.sum(10, 20));
+//         System.out.println("Sum of 2 doubles: " + obj.sum(10.5, 20.5));
+//         System.out.println("Sum of 3 integers: " + obj.sum(10, 20, 30));
+        
+//     }
+// }
