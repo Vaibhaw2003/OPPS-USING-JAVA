efefafffb05acd413e10626a03ac8b06eff3c25b
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-27 23:20:57 +0530

    TAKING INPUT FROM THE USE

--- UNIT01/Demo.java
+++ UNIT01/Demo.java
@@ -376,3 +376,20 @@ class Demo {
 //         dog.sound(); // Calls Dog's sound method
 //     }
 // }
+
+
+//----------- TAKING INPUT FROM THE USE -------
+
+import java.util.*;;
+class Demo{
+    public static void main(String[] args){
+        System.out.println("enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i =0; i<=num; i++){
+            System.out.println(i);
+        }
+
+    }
+}
