0952fcccfdbe6c45c54e16c59454bd0017da78de
VAIBHAW SINGH <vaibhawrajput05@gmail.com>
2026-07-14 23:30:30 +0530

    Update Demo.java

--- ADV-JAVA-CLASS/Demo.java
+++ ADV-JAVA-CLASS/Demo.java
@@ -12,7 +12,6 @@
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         System.out.println("enter the number : ");
-
 //         int a = sc.nextInt(),
 //          b = sc.nextInt(),
 //          c = sc.nextInt();
