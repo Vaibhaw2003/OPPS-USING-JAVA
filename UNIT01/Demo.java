fc9ec33662cbcd9988fa4fa9929b9868a50e70ba
VAIBHAW SINGH <vaibhawrajput05@gmail.com>
2026-08-20 22:38:03 +0530

    Update Demo.java

--- UNIT01/Demo.java
+++ UNIT01/Demo.java
@@ -137,7 +137,6 @@ class Demo {
         for (int i = str.length() - 1; i >= 0; i--) {
             rev = rev + str.charAt(i);
         }
-
         System.out.println("Reversed string: " + rev);
     }
 }
